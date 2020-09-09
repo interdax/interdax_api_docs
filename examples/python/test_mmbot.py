5dc51fc0585c38e458e986ed597551460861c40b
jordanxyx <64850767+jordanxyx@users.noreply.github.com>
2020-09-08 18:58:46 -0700

    Update test_mmbot.py

--- examples/python/test_mmbot.py
+++ examples/python/test_mmbot.py
@@ -1,27 +1,33 @@
 import unittest
 import os
 
-test_args = ["python",
-             "mmbot.py",
-             "-as", os.environ['API_KEY_SECRET'],
-             "-ak", os.environ['API_KEY_ID'],
-             "-t",
-             "--market-maker"
-             ]
-
-if 'TEST' in os.environ.keys():
-    print(os.environ['TEST'])
-    if os.environ['TEST'] == 'true':
-        test_args.append("-t")
 
 def bash_cmd(cmd='ls'):
     with os.popen(cmd) as do:
         output = do.read()
         return output
 
+
 class TestBot(unittest.TestCase):
+    
+    test_args = ["python",
+                 "mmbot.py",
+                 "-as", os.environ['API_KEY_SECRET'],
+                 "-ak", os.environ['API_KEY_ID'],
+                 "-t",
+                 "--market-maker"
+                 ]
+
+    @classmethod
+    def setUpClass(cls):
+
+        if 'TEST' in os.environ.keys():
+            print(os.environ['TEST'])
+            if os.environ['TEST'] == 'true':
+                cls.test_args.append("-t")
+
     def test_market_maker(self):
-        r = bash_cmd(' '.join(test_args))
+        r = bash_cmd(' '.join(self.test_args))
         self.assertIn('Test passed', r)
 
 
